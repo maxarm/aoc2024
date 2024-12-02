a9a1aa3f8731bfc2dc7041c3112578d5bb46e7e2
maxarm <markus.armbruckner@outlook.com>
2024-12-02 20:55:03 +0100

    solved 2b

--- day2/d02.py
+++ day2/d02.py
@@ -2,16 +2,13 @@ import utils
 from typing import List
 
 
-def get_number_of_safe_reports(read_test_data: bool = False) -> int:
+def get_number_of_safe_reports(tolerate_a_bad_level: bool, read_test_data: bool) -> int:
     lines = utils.read_data(2, read_test_data)
     result = 0
     for line in lines:
         nums = get_numbers_of_string(line)
-        if is_increasing_safely(nums):
-            print(f"{nums} is increasing safely")
-            result += 1
-        elif is_decreasing_safely(nums):
-            print(f"{nums} is decreasing safely")
+        if (is_increasing_safely(nums, tolerate_a_bad_level) or
+                is_decreasing_safely(nums, tolerate_a_bad_level)):
             result += 1
 
     return result
@@ -21,7 +18,51 @@ def get_numbers_of_string(line: str) -> List[int]:
     return [int(x) for x in line.split(" ")]
 
 
-def is_increasing_safely(nums: List[int]) -> bool:
+def is_increasing_safely(nums: List[int], tolerate_a_bad_level: bool) -> bool:
+    if is_increasing_safely_strict(nums):
+        print(f"{nums}: Increasing safely without removing any level.")
+        return True
+
+    if tolerate_a_bad_level and is_increasing_safely_tolerantly(nums):
+        print(f"{nums}: Increasing safely by removing a level.")
+        return True
+
+    return False
+
+
+def is_decreasing_safely(nums: List[int], tolerate_a_bad_level: bool) -> bool:
+    if is_decreasing_safely_strict(nums):
+        print(f"{nums}: Decreasing safely without removing any level.")
+        return True
+
+    if tolerate_a_bad_level and is_decreasing_safely_tolerantly(nums):
+        print(f"{nums}: Decreasing safely by removing a level.")
+        return True
+
+    return False
+
+
+def remove_a_list_element(nums: List[int], index: int) -> List[int]:
+    return nums[:index] + nums[index+1:]
+
+
+def is_increasing_safely_tolerantly(nums: List[int]) -> bool:
+    for i in range(0, len(nums)):
+        new_list = remove_a_list_element(nums, i)
+        if is_increasing_safely_strict(new_list):
+            return True
+    return False
+
+
+def is_decreasing_safely_tolerantly(nums: List[int]) -> bool:
+    for i in range(0, len(nums)):
+        new_list = remove_a_list_element(nums, i)
+        if is_decreasing_safely_strict(new_list):
+            return True
+    return False
+
+
+def is_increasing_safely_strict(nums: List[int]) -> bool:
     return all([
         x < y and
         abs(x-y) > 0 and
@@ -30,7 +71,7 @@ def is_increasing_safely(nums: List[int]) -> bool:
     ])
 
 
-def is_decreasing_safely(nums: List[int]) -> bool:
+def is_decreasing_safely_strict(nums: List[int]) -> bool:
     return all([
         x > y and
         abs(x-y) > 0 and
--- day2/test_d02.py
+++ day2/test_d02.py
@@ -1,7 +1,13 @@
 import d02
 
 def test_d02a_testdata():
-    assert d02.get_number_of_safe_reports(True) == 2
+    assert d02.get_number_of_safe_reports(False, True) == 2
 
 def test_d02a_data():
-    assert d02.get_number_of_safe_reports(False) == 390
+    assert d02.get_number_of_safe_reports(False, False) == 390
+
+def test_d02b_testdata():
+    assert d02.get_number_of_safe_reports(True, True) == 5
+
+def test_d02b_data():
+    assert d02.get_number_of_safe_reports(True, False) == 4
