7f51a8cadc08c55363e7fa4b6509d9953de536df
maxarm <markus.armbruckner@outlook.com>
2024-12-02 20:20:58 +0100

    refactor

--- day2/d02.py
+++ day2/d02.py
@@ -1,7 +1,8 @@
 import utils
 from typing import List
 
-def get_number_of_safe_reports(read_test_data: bool=False) -> int:
+
+def get_number_of_safe_reports(read_test_data: bool = False) -> int:
     lines = utils.read_data(2, read_test_data)
     result = 0
     for line in lines:
@@ -15,27 +16,24 @@ def get_number_of_safe_reports(read_test_data: bool=False) -> int:
 
     return result
 
-def get_numbers_of_string(line: str) -> List[int]:
-    str_nums = line.split(" ")
-    nums = [int(x) for x in str_nums]
-    return nums
-    
-def is_increasing_safely(nums: List[int]) -> bool:
-    for i in range(len(nums)-1):
-        num_diff = abs(nums[i] - nums[i+1])
-        if nums[i] >= nums[i+1] or num_diff < 1 or num_diff > 3:
-            return False
 
-    return True
+def get_numbers_of_string(line: str) -> List[int]:
+    return [int(x) for x in line.split(" ")]
+
+
+def is_increasing_safely(nums: List[int]) -> bool:
+    return all([
+        x < y and
+        abs(x-y) > 0 and
+        abs(x-y) < 4
+        for x, y in zip(nums, nums[1:])
+    ])
+
 
 def is_decreasing_safely(nums: List[int]) -> bool:
-    for i in range(len(nums)-1):
-        num_diff = abs(nums[i] - nums[i+1])
-        if nums[i] <= nums[i+1] or num_diff < 1 or num_diff > 3:
-            return False
-
-    return True
-
-
-
-
+    return all([
+        x > y and
+        abs(x-y) > 0 and
+        abs(x-y) < 4
+        for x, y in zip(nums, nums[1:])
+    ])
